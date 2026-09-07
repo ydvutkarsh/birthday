e898bb49ee0a4400d9d2dd8b3de9407246bc51c0
Utkarsh Yadav <utkarsh9415528079@gmail.com>
2026-09-07 21:01:55 +0530

    fix guest login seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,23 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        User::updateOrCreate(
+            ['username' => 'omsha'],
+            [
+                'name' => 'Omsha',
+                'email' => 'omsha@example.com',
+                'password' => Hash::make('om@123'),
+                'role' => 'guest',
+                'status' => 'active',
+            ]
+        );
     }
 }
